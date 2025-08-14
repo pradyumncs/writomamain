2dddbb4336034703e033463fa1fce60376011d24
pradyumncs <pradyumncs@gmail.com>
2025-08-14 12:26:30 +0530

    xz

--- middleware.ts
+++ middleware.ts
@@ -7,5 +7,17 @@ export { auth as middleware } from "auth"
 
 // Read more: https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api/auth (NextAuth.js routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - homedash.png (the image you want to be public)
+     * - / (the root path)
+     * - /colleges/ (the colleges images)
+     */
+    "/((?!api/auth|_next/static|_next/image|favicon.ico|homedash.png|logo.png|/logo.png|colleges/|/colleges/|$).*)",
+  ],
 }
